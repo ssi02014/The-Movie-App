9f312ba5dfcaccc3d0caac7d1bead55e9cd21fab
JeonMinJae <ssi02014@naver.com>
2021-01-21 00:46:03 +0900

    Implement: Header Login/Logout 버튼 바꾸기 구현

--- client/src/components/views/HomePage/HomePage.js
+++ client/src/components/views/HomePage/HomePage.js
@@ -2,32 +2,55 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { Link, withRouter } from 'react-router-dom';
 import Header from '../Header';
+import { API_URL, API_KEY } from '../../../config/Config';
 
 const HomePage = (props) => {
 
     //홈페이지 들어오면 useEffect를 실행 1번만
     useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => console.log(response.data));
+        const endpoint = `${API_URL}movie/popular/`;
     }, []);
 
-    const onClickHandler = () => {
-        axios.get('/api/users/logout')
-        .then(response => {
+    // const onClickHandler = () => {
+    //     axios.get('/api/users/logout')
+    //     .then(response => {
 
-            console.log(response);
+    //         console.log(response);
 
-            if (response.data.success) {
-                props.history.push('/login');
-            } else {
-                alert("로그아웃하는데 실패하였습니다.");
-            }
-        })
-    }
+    //         if (response.data.success) {
+    //             props.history.push('/login');
+    //         } else {
+    //             alert("로그아웃하는데 실패하였습니다.");
+    //         }
+    //     })
+    // }
 
     return (
         <>
             <Header></Header>
+            <div style={{
+                width: '100%',
+                margin: '0',
+            }}>
+
+                {/* Main Image */}
+                <div style={{
+                    width: '85%',
+                    margin: '1rem auto',
+                }}>
+                    <h2>Movies by latest</h2>
+                    <hr />
+
+                    {/* Movie Grid Card */}
+                    
+                </div>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}>
+                    <button>Load More</button>
+                </div>
+            </div>
         </>
 
     );
--- client/src/config/Config.js
+++ client/src/config/Config.js
@@ -1,2 +1,3 @@
 export const API_URL = 'https://api.themoviedb.org/3/';
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+export const API_KEY = '04c35731a5ee918f014970082a0088b1';
