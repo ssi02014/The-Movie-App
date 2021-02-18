e12fae15d4e30b1eeded5893ee1fd7b3253c50a5
JeonMinJae <ssi02014@naver.com>
2021-02-19 04:25:35 +0900

    Style(minjae): Main Image backgroundPosition update

--- client/src/components/views/HomePage/Sections/MainImage.js
+++ client/src/components/views/HomePage/Sections/MainImage.js
@@ -6,7 +6,7 @@ const MainImage = ({image, title, desc}) => {
             background: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 39%, rgba(0,0,0,0) 41%, rgba(0, 0, 0, 0.65) 100%), url('${image}'), 100%`,
             height: '500px',
             backgroundSize: '100%, cover',
-            // backgroundPosition: 'center, center',
+            backgroundPosition: 'center, center',
             width: '100%',
             position: 'relative',
             marginTop: '50px'
